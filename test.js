ea591752ea39ac1ee6b85655bddcc9e2422b053c
suoyue <suoyuesmile@163.com>
2017-10-15 22:54:22 +0800

    add demo

---
+++ test.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//         $(this).addClass("color");
+//     });
+//     $("h2").mouseover(function() {
+//         $(this).hide();
+//     });
+// });
+
+$(function() {
+    $("h1").click(function() {
+        $(this).addClass("color");
+    });
+});
